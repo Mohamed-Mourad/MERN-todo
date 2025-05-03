97291e97c8c05233cac2a46572a17015f6f44312
Mohamed Mourad <67578210+Mohamed-Mourad@users.noreply.github.com>
2025-05-04 02:48:26 +0300

    Update PrivateRoute.js

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 // src/components/PrivateRoute.js
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext'; // Import useAuth hook
+import { useAuth } from '../context/AuthContext';
 
 const PrivateRoute = () => {
   const { isAuthenticated, isLoading } = useAuth(); // Get auth state from context
