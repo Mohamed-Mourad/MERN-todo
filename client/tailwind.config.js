176ce804b8c63ace75923b8ea5138fa7f6cda621
Mohamed Mourad <67578210+Mohamed-Mourad@users.noreply.github.com>
2025-05-04 02:21:17 +0300

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/JSX/TS/TSX files in the src directory
-    "./public/index.html"       // Include the main HTML file if needed
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
